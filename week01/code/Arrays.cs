211c5b7f4ecf99e44ade9e4bd74127b815ff4c0b
jameseleight <jleight@byupathway.edu>
2026-03-02 01:06:56 -0800

    Updates for W01 assignments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // This method will return an array of doubles.  The approach is to first create a new array of size length.
+        // Then run a for loop from index i 1 to length.  Then at each step
+        // there will be a line to calculate the multiple of the i * number and this value will be added to the array.
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = (i + 1) * number;
+        }
+        // Debug.WriteLine("<List>{" + string.Join(", ", multiples) + "}");
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // The approach used to implement this method is to first load the items at the end of the list into a temporary list.
+        // This is a GetRange method.  Then RemoveRange to remove that copied list from the original list data.  Then InsertRange to put that list at the front.
+        List<int> tempStore = new List<int>();
+        tempStore = data.GetRange(data.Count - amount, amount);
+        // Debug.WriteLine("<List>{" + string.Join(", ", tempStore) + "}");
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0,tempStore);
+        // Debug.WriteLine("<List>{" + string.Join(", ", data) + "}");
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -6,11 +6,30 @@ public static class ArraySelector
         var l2 = new[] { 2, 4, 6, 8, 10};
         var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
         var intResult = ListSelector(l1, l2, select);
-        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+        Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // expected is <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int index1 = 0;
+        int index2 = 0;
+        int resultIndex = 0;
+        int[] listResult = new int[select.Length];
+        foreach (var selectorValue in select)
+        {
+            if (selectorValue == 1)
+            {
+                listResult[resultIndex] = list1[index1];
+                index1++;
+                resultIndex++;
+            }
+            else // selectorValue == 2
+            {
+                listResult[resultIndex] = list2[index2];
+                index2++;
+                resultIndex++;
+            }
+        }
+        return listResult;
     }
 }
